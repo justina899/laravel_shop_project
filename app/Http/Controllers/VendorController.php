9888ee93e25408c9b8eaafa5eb2982efefab3860
Justina <justina.babrauskiene@gmail.com>
2024-05-28 13:09:49 +0300

    VendorDashboard method created in VendorController

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class VendorController extends Controller
 {
-    //
+    public function VendorDashboard(){
+        return view('vendor.vendor_dashboard');
+    } //End method
 }
