668b7ce35ae5a9f9c8da1461ec7c126bdd602273
Justina <justina.babrauskiene@gmail.com>
2024-05-28 12:12:17 +0300

    DatabaseSeeder.php: run method modified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        
+        $this->call(UserTableSeeder::class);
+        User::factory(8)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
