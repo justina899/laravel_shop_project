b010cd90971f4d86cfd8438de1ebe83acda18a1d
Justina <justina.babrauskiene@gmail.com>
2024-05-30 12:08:35 +0300

    middleware  registered

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VendorController;
+use App\Http\Middleware\Role;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,5 +22,13 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
+Route::middleware(['auth', 'role:vendor'])->group(function () {
+    Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
+});
+
+//registered middleware
+Route::get('/admin/dashboard', function () {})->middleware(Role::class);
+Route::get('/vendor/dashboard', function () {})->middleware(Role::class);
