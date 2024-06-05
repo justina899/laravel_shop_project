64875c8db5e4686a6f066b6a70dfb4575fc13b1d
Justina <justina.babrauskiene@gmail.com>
2024-06-05 14:11:25 +0300

    edited page

--- resources/views/admin/admin_change_password.blade.php
+++ resources/views/admin/admin_change_password.blade.php
@@ -6,13 +6,13 @@
 <div class="page-content"> 
     <!--breadcrumb-->
     <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-        <div class="breadcrumb-title pe-3">Admin Change Password</div>
+        <div class="breadcrumb-title pe-3">Admin change password</div>
         <div class="ps-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb mb-0 p-0">
                     <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                     </li>
-                    <li class="breadcrumb-item active" aria-current="page">Change Password</li>
+                    <li class="breadcrumb-item active" aria-current="page">Change password</li>
                 </ol>
             </nav>
         </div>
@@ -20,7 +20,7 @@
         </div>
     </div>
     <!--end breadcrumb-->
-    
+
     <div class="container">
         <div class="main-body">
             <div class="row">
@@ -39,36 +39,34 @@
 
                             <div class="row mb-3">
                                 <div class="col-sm-3">
-                                    <h6 class="mb-0">Old Password</h6>
+                                    <h6 class="mb-0">Old password</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="password" name="old_password" class="form-control @error('old_password') is-invalid @enderror" id="current_password"   placeholder="Old Password" />
-
+                                    <input type="password" name="old_password" class="form-control @error('old_password') is-invalid @enderror" id="current_password" placeholder="Old Password" />
                                     @error('old_password')
-                                    <span class="text-danger">{{ $message }}</span>
+                                        <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <div class="col-sm-3">
-                                    <h6 class="mb-0">New Password</h6>
+                                    <h6 class="mb-0">New password</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="password" name="new_password" class="form-control @error('new_password') is-invalid @enderror" id="new_password"   placeholder="New Password" />
-
+                                    <input type="password" name="new_password" class="form-control @error('new_password') is-invalid @enderror" id="new_password" placeholder="New Password" />
                                     @error('new_password')
-                                    <span class="text-danger">{{ $message }}</span>
+                                        <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <div class="col-sm-3">
-                                    <h6 class="mb-0">Confirm New Password</h6>
+                                    <h6 class="mb-0">Confirm new password</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="password" name="new_password_confirmation" class="form-control" id="new_password_confirmation"   placeholder="Confirm New Password" /> 
+                                    <input type="password" name="new_password_confirmation" class="form-control" id="new_password_confirmation" placeholder="Confirm New Password" /> 
                                 </div>
                             </div>
 
