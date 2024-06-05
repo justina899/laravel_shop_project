57426d97ccc07fa9a66285d720552d4110d0bacc
Justina <justina.babrauskiene@gmail.com>
2024-06-05 11:12:00 +0300

    admin dashboard: added route in Profile

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ require __DIR__.'/auth.php';
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 });
 
 //Vendor dashboard
