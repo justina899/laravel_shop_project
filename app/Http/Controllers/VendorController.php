0139e9461b74a885cefe8c34392ec32543f709ec
Justina <justina.babrauskiene@gmail.com>
2024-06-07 19:09:26 +0300

    VendorDestroy method created

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class VendorController extends Controller
 {
@@ -13,5 +14,15 @@ class VendorController extends Controller
 
     public function VendorLogin(){
         return view('vendor.vendor_login');
+    } //End method
+
+    public function VendorDestroy(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/vendor/login');
     } //End method
 }
