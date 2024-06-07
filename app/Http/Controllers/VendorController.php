640e38f9bd45a0164ee36405857c8d7d9c30cb31
Justina <justina.babrauskiene@gmail.com>
2024-06-07 19:25:16 +0300

    VendorProfile method created

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class VendorController extends Controller
 {
@@ -24,5 +26,11 @@ class VendorController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/vendor/login');
+    } //End method
+
+    public function VendorProfile(){
+        $id = Auth::user()->id;
+        $vendorData = User::find($id);
+        return view('vendor.vendor_profile_view', compact('vendorData'));
     } //End method
 }
