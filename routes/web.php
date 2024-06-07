02e600b7825cc13f2e5ea6328823d7b88772bfa3
Justina <justina.babrauskiene@gmail.com>
2024-06-07 20:48:39 +0300

    vendor.profile.store route created

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ Route::middleware(['auth', 'role:vendor'])->group(function () {
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
     Route::get('/vendor/logout', [VendorController::class, 'VendorDestroy'])->name('vendor.logout');
     Route::get('/vendor/profile', [VendorController::class, 'VendorProfile'])->name('vendor.profile');
+    Route::post('/vendor/profile/store', [VendorController::class, 'VendorProfileStore'])->name('vendor.profile.store');
 });
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
