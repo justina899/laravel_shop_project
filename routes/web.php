ed354c1ff339ee97adf7dcc6317f8a066590338d
Justina <justina.babrauskiene@gmail.com>
2024-06-07 19:07:57 +0300

    vendor.logout route created

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 //Vendor dashboard
 Route::middleware(['auth', 'role:vendor'])->group(function () {
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
+    Route::get('/vendor/logout', [VendorController::class, 'VendorDestroy'])->name('vendor.logout');
 });
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
