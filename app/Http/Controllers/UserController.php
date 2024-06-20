e1e65c97e7403a1bc8aaf418545ce96202da2972
Justina <justina.babrauskiene@gmail.com>
2024-06-20 17:54:41 +0300

    UserLogout created

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,9 +43,17 @@ class UserController extends Controller
         );
 
         return redirect()->back()->with($notification);
-    } // End Mehtod 
+    } //End Method 
     
     
-    
+    public function UserLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    } //End Method 
      
 }
