7494b9fa3ded872909a04444016fcde1fba17549
Justina <justina.babrauskiene@gmail.com>
2024-06-20 16:53:45 +0300

    index updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,6 +45,8 @@
                             <div class="tab-pane fade active show" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
                                 <div class="card">
                                     <div class="card-header">
+                                    <div class="form-group col-md-12">
+                                        <img id="showImage" src="{{ (!empty($userData->photo)) ? url('upload/user_images/'.$userData->photo) : url('upload/no_image.jpg') }}" alt="User" class="rounded-circle p-1 bg-primary" width="110">
                                         <h3 class="mb-0">Hello, {{ Auth::user()->name }}!</h3>
                                     </div>
                                     <div class="card-body">
