f0047dc1667faeabce8150ae34b101cc3fb93592
Justina <justina.babrauskiene@gmail.com>
2024-06-25 17:05:26 +0300

    brand page design1

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VendorController;
 use App\Http\Middleware\Role;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Backend\BrandController;
+
 
 Route::get('/', function () {
     return view('frontend.index');
@@ -52,3 +54,13 @@ Route::middleware(['auth', 'role:vendor'])->group(function () {
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
 Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);
+
+Route::middleware(['auth','role:admin'])->group(function() {
+
+    // Brand All Route 
+   Route::controller(BrandController::class)->group(function(){
+       Route::get('/all/brand' , 'AllBrand')->name('all.brand');
+   
+   });
+   
+}); // End Middleware 
