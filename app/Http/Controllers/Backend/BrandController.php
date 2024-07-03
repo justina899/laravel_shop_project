a404cf82a9cc9bb23c0068990deb064ca48c0a2c
Justina <justina.babrauskiene@gmail.com>
2024-07-03 15:36:29 +0300

    UpdateBrand method created

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -46,5 +46,52 @@ class BrandController extends Controller
    public function EditBrand($id){
         $brand = Brand::findOrFail($id);
         return view('backend.brand.brand_edit', compact('brand'));
-    } // End Method 
+    } //End Method 
+
+    public function UpdateBrand(Request $request){
+
+        $brand_id = $request->id;
+        $old_img = $request->old_image;
+
+        if ($request->file('brand_image')) {
+
+            $image = $request->file('brand_image');
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img = $img->resize(300, 300);
+            $img->toJpeg(80)->save('upload/brand/'.$name_gen);
+            $save_url = 'upload/brand/'.$name_gen;
+
+            if (file_exists($old_img)) {
+                unlink($old_img);
+            }
+
+            Brand::findOrFail($brand_id)->update([
+                'brand_name' => $request->brand_name,
+                'brand_slug' => strtolower(str_replace(' ', '-', $request->brand_name)),
+                'brand_image' => $save_url, 
+            ]);
+
+            $notification = array(
+                'message' => 'Brand updated with image successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->route('all.brand')->with($notification); 
+        } else {
+
+            Brand::findOrFail($brand_id)->update([
+                'brand_name' => $request->brand_name,
+                'brand_slug' => strtolower(str_replace(' ', '-', $request->brand_name)), 
+            ]);
+
+            $notification = array(
+                'message' => 'Brand updated without image successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->route('all.brand')->with($notification); 
+        } 
+    } //End Method 
 }
