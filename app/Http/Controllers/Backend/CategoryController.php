70898af2981fea7adbe4d77ba2b78947181819a8
Justina <justina.babrauskiene@gmail.com>
2024-08-04 14:03:02 +0300

    StoreCategory method created

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -18,4 +18,27 @@ class CategoryController extends Controller
     public function AddCategory(){
         return view('backend.category.category_add');
     } //End Method
+
+    public function StoreCategory(Request $request){
+        $image = $request->file('category_image');
+        $manager = new ImageManager(new Driver());
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        $img = $manager->read($image);
+        $img = $img->resize(120, 120);
+        $img->toJpeg(80)->save('upload/category/'.$name_gen);
+        $save_url = 'upload/category/'.$name_gen;
+
+        Category::insert([
+            'category_name' => $request->category_name,
+            'category_slug' => strtolower(str_replace(' ', '-', $request->category_name)),
+            'category_image' => $save_url, 
+        ]);
+
+        $notification = array(
+            'message' => 'Category inserted successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.category')->with($notification);
+   } //End Method 
 }
