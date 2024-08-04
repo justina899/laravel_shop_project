ba9ef8918d8f1fe00b1a20c5836168a89d3da246
Justina <justina.babrauskiene@gmail.com>
2024-08-04 13:19:46 +0300

    AllCategory method created

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class CategoryController extends Controller
 {
-    //
+    public function AllCategory(){
+        $categories = Category::latest()->get();
+        return view('backend.category.category_all', compact('categories'));
+    } //End Method 
 }
