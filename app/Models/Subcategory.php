e92d4cbb75c45958e1276be0f017d18f582f8791
Justina <justina.babrauskiene@gmail.com>
2024-08-09 16:48:25 +0300

    category method added

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -9,4 +9,8 @@ class Subcategory extends Model
 {
     use HasFactory;
     protected $guarded = []; //to make our all fields fillable
+
+    public function category(){
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
