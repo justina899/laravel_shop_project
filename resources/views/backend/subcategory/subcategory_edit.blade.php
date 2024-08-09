91b41991f8bb85a30cd8e8d50b98818fa2f3aa21
Justina <justina.babrauskiene@gmail.com>
2024-08-09 17:25:10 +0300

    condition added to foreach

--- resources/views/backend/subcategory/subcategory_edit.blade.php
+++ resources/views/backend/subcategory/subcategory_edit.blade.php
@@ -38,7 +38,7 @@
 			                                <option selected="">Open this select menu</option>
 
 			                                    @foreach($categories as $category)
-		 	                                        <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+		 	                                        <option value="{{ $category->id }}" {{ $category->id == $subcategory->category_id ? 'selected' : ''}}>{{ $category->category_name }}</option>
 		 	                                    @endforeach
 
 								        </select>
