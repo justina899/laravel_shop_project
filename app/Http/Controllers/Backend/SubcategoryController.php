3957ae7cc68811c601a07bbd646d41567188a0c0
Justina <justina.babrauskiene@gmail.com>
2024-08-09 14:40:55 +0300

    AllSubcategory method created

--- app/Http/Controllers/Backend/SubcategoryController.php
+++ app/Http/Controllers/Backend/SubcategoryController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Subcategory;
+use App\Models\Category;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class SubcategoryController extends Controller
 {
-    //
+    public function AllSubcategory(){
+        $subcategories = Subcategory::latest()->get();
+        return view('backend.subcategory.subcategory_all', compact('subcategories'));
+    } //End method 
 }
